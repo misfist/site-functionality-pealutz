d200331df1baef006040712a82179efefb90b1d1
P. E. A. Lutz <pea@misfist.com>
2026-06-10 21:28:31 -0400

    Build

--- src/app/blocks/build/bindings.asset.php
+++ src/app/blocks/build/bindings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-core-data', 'wp-data', 'wp-date', 'wp-i18n'), 'version' => '64464ade088722932f37');
+<?php return array('dependencies' => array('wp-blocks', 'wp-core-data', 'wp-data', 'wp-date', 'wp-i18n'), 'version' => '839947becf5862969d41');
--- src/app/blocks/build/index.asset.php
+++ src/app/blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ec7b2e4e8c7ac0c00443');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'fec9efa3cc7871434b58');
