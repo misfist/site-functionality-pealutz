09e9f4a1f0961c9e55af4aeba692a9db55b0a421
P. E. A. Lutz <pea@misfist.com>
2026-06-16 02:37:56 -0400

    Add `is_filter `field

--- src/app/taxonomies/class-project-tag.php
+++ src/app/taxonomies/class-project-tag.php
@@ -46,6 +46,7 @@ class Project_Tag extends Taxonomy {
 		parent::__construct();
 
 		\add_action( 'init', array( $this, 'rewrite_rules' ), 10, 0 );
+		\add_filter( 'rwmb_meta_boxes', array( $this, 'register_fields' ) );
 	}
 
 	/**
@@ -56,4 +57,29 @@ class Project_Tag extends Taxonomy {
 	 * @return void
 	 */
 	public function rewrite_rules(): void {}
+
+	/**
+	 * Register Fields
+	 * 
+	 * @since 1.0.14
+	 *
+	 * @param array $meta_boxes
+	 * @return array
+	 */
+	public function register_fields( array $meta_boxes ): array {
+		$meta_boxes[] = array(
+			'taxonomies' => array( self::$taxonomy['id'] ),
+			'title'      => __( '', 'site-functionality' ),
+			'id'         => 'details',
+			'fields'     => array(
+				array(
+					'type' => 'switch',
+					'id'   => 'is_filter',
+					'name' => __( 'Filter', 'site-functionality' ),
+				),
+			),
+		);
+
+		return $meta_boxes;
+	}
 }
